a5fe702329b693a673ee44b7f59fda6fa3d34335
demixitho3 <dennismanuel000@gmail.com>
2021-09-04 14:28:22 -0500

    fix (database): hackfix levitate drakes in caverns of time

---
+++ sql/updates/04-09-2021/04_09_2021_fix_fly_drake_caverns_of_time.sql
@@ -0,0 +1,2 @@
+-- Tis is a insane hackfix, simulate fly creature momentary hackfix need rework flags in tis source for fliying creatures
+REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES (19918, 0, 0, 33554432, 0, 0, NULL);
