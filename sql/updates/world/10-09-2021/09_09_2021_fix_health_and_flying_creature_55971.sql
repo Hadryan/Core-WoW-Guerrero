4e13af6fe30b7756044fc858abd8f1fabdb8b15d
demixitho3 <dennismanuel000@gmail.com>
2021-09-10 07:54:58 -0500

    fix (database): fix set healt and fliying in creature 55971

---
+++ sql/updates/world/10-09-2021/09_09_2021_fix_health_and_flying_creature_55971.sql
@@ -0,0 +1,2 @@
+UPDATE creature_template SET Health_mod = '1480' WHERE entry = '55971';
+REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES (55971, 0, 0, 33554432, 0, 0, NULL);
